c07c5a6cf8c191b7deade2d103566baf8b615721
winstijn <winstijn@hotmail.nl>
2023-05-04 10:10:40 +0200

    Added auto port selection on macOS

--- data_collection_interface/util.py
+++ data_collection_interface/util.py
@@ -1,6 +1,7 @@
 import sys
 import glob
 import serial
+import platform
 
 def serial_ports():
     """ Lists serial port names
@@ -29,3 +30,19 @@ def serial_ports():
         except (OSError, serial.SerialException):
             pass
     return result
+
+def auto_select_serial_port() -> str:
+    ports = serial_ports()
+    os = platform.system()
+    if os == "Windows":
+        return ports[0]
+    elif os == "Linux":
+        return ports[0]
+    elif os == "Darwin":
+        # Try to find the port that has usbmoddem in it
+        for port in ports:
+            if "usbmodem" in port:
+                return port
+    
+    return ports[0] # Otherwise just return the first port
+    
